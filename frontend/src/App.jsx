4599e2afacc034eee87961c4b06ee63a8538e40e
dhruv619singha <priyanshuntak@gmail.com>
2025-01-16 11:05:33 +0530

    buy coupon route

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from './pages/Home'
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
